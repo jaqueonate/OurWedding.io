cbbf06246faf3ea68c084ffa08d1f004209847e2
jaqueonate <jacqueline.o@live.com>
2025-07-10 00:50:03 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -227,6 +227,7 @@ btnConsultar.addEventListener('click', async (event) => {
   ShowInvitation.style.display = 'none';
 
   const input = document.getElementById('id_invitado');
+  const nombre = input.value.trim();
   const apiURL = `https://ourwedding-io.onrender.com/api/${nombre}`;
 
   try {
